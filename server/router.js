f4781a39f0c75c623f2c8babdcdec00e1cc7f252
Samiya Kazi <samiya.kazi09@gmail.com>
2022-08-15 15:00:44 +0600

    added more to server structure

--- server/router.js
+++ server/router.js
@@ -1,4 +1,9 @@
 const router = require('express').Router();
 
+router.post('/organization/create');
+router.post('/login');
+router.get('/organization/:id/users');
+router.delete('/organization/:id/user/:userId');
+
 
 module.exports = router;
