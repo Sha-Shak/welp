1f9adc08bfea08ae460004d4fa8165f075f92735
Sha-Shak <shahriar.shakhawat@gmail.com>
2022-08-15 13:30:27 +0600

    react setup with daisy

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl text-pink-600 underline">Hii1</h1>
+      <button>B1</button>
+      <button className="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+        Button
+      </button>
+    </div>
+  );
+}
+
+export default App;
