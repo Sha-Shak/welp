881678594e71ae3427a7404a36719f5830cb96de
Jesse Hardage <j.hardage@pm.me>
2022-08-15 13:08:24 +0100

    ft: added socket end pts for video calling

--- server/index.js
+++ server/index.js
@@ -57,6 +57,19 @@ io.on('connection', (socket) => {
   socket.on('send_message', (data) => {
     socket.to(data.room_id).emit('receive_message', data);
   })
+
+  // video calling points
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('callEnded');
+  });
+
+  socket.on('callUser', (data) => {
+    io.to(data.userToCall).emit('callUser', {signal: data.signalData, from: data.from, name: data.name});
+  });
+
+  socket.on('answerCall', (data) => {
+    io.to(data.to).emit('callAccepted', data.signal)
+  });
 })
 
 
