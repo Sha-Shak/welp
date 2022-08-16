93f2cfb4434b16216ac65f9db2c887bc8b59d0ed
Samiya Kazi <samiya.kazi09@gmail.com>
2022-08-16 14:42:39 +0600

    added messaging routes

--- server/router.js
+++ server/router.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const organizationController = require('./controllers/organization');
 const userController = require('./controllers/user');
+const chatController = require('./controllers/chat');
 const authMiddleware = require('./middleware/auth');
 
 router.post('/organization/create', organizationController.createNewOrganization);
@@ -12,5 +13,9 @@ router.post('/login', userController.login);
 router.get('/user/:id', authMiddleware, userController.getProfile);
 router.put('/user', authMiddleware, userController.editProfile);
 router.get('/suggestion', authMiddleware, userController.getSuggestions);
+
+
+router.get('/chats', authMiddleware, chatController.getUserChats);
+router.get('/chat/:id', authMiddleware, chatController.getChatMessages);
 
 module.exports = router;
