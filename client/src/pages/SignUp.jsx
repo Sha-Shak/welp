d7657db8c99264e94b5362caa067fac5b71841b5
Sha-Shak <shahriar.shakhawat@gmail.com>
2022-08-16 14:47:42 +0600

    signup form

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -2,9 +2,16 @@ import React from "react";
 import SignupForm from "../Components/Signup/SignupForm";
 function SignUp() {
   return (
-    <>
+    <div className="flex justify-around">
       <SignupForm />
-    </>
+      <div>
+        <img
+          src="https://res.cloudinary.com/dgn4bscl4/image/upload/v1660635169/login_don2mi.png"
+          className=""
+          alt=""
+        />
+      </div>
+    </div>
   );
 }
 
