c2fb857d22c3ed83cef884e65d3be1eb54791896
Samiya Kazi <samiya.kazi09@gmail.com>
2022-08-17 20:36:17 +0600

    fixed auth middleware

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -4,16 +4,21 @@ const secret = process.env.JWT_SECRET;
 
 const authMiddleware = async (req, res, next) => {
   try {
-    const token = req.get('Authorization').split(' ')[1];
-    const { id } = jwt.verify(token, secret);
-    const user = await getUserById(id);
-
-    if(user.length === 1) {
-      req.user = user[0];
-      next();
+    if (req.get('Authorization')) {
+      const token = req.get('Authorization').split(' ')[1];
+      const { id } = jwt.verify(token, secret);
+      const user = await getUserById(id);
+  
+      if(user.length === 1) {
+        req.user = user[0];
+        next();
+      } else {
+        res.status(401).send('You are not logged in!');
+      }
     } else {
       res.status(401).send('You are not logged in!');
     }
+    
   } catch (error) {
     res.status(500);
     console.log(error);
