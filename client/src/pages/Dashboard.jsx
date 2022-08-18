cced4f89a2f35542c4049f48171179cb3a93270b
Sha-Shak <shahriar.shakhawat@gmail.com>
2022-08-19 01:59:33 +0600

    ux improvemenr

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,14 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import { getUsers, recommendUsers } from "../actions/users.action.js";
 import Banner from "../Components/Banner/Banner";
 import UserCard from "../Components/UserCard/UserCard";
 
-
 function Dashboard() {
   const dispatch = useDispatch();
   const user = JSON.parse(localStorage.getItem("data"));
-
 
   useEffect(() => {
     // console.log("use effect");
@@ -16,7 +15,6 @@ function Dashboard() {
   }, [dispatch]);
   const fetchUsers = useSelector((state) => state.allUsers);
 
-  
   const [userList, setUserList] = useState(fetchUsers);
   console.log("fetch users", fetchUsers);
   useEffect(() => {
@@ -26,7 +24,6 @@ function Dashboard() {
       dispatch(recommendUsers());
     }
   }, [userList, dispatch]);
-
 
   const orgUsers = fetchUsers.filter((fetchUser) => fetchUser.id !== user.id);
   // const [orgUsers, setOrgUsers] = useState(fetchUsers);
@@ -43,9 +40,11 @@ function Dashboard() {
             <h2 className="text-dark text-3xl mt-16 mb-10 items-center">
               You don't have any users yet
             </h2>
-            <button className="btn btn-primary mx-1 rounded-full right-0">
-              Add your first user
-            </button>
+            <Link to="/add-user">
+              <button className="btn btn-primary mx-1 rounded-full right-0">
+                Add your first user
+              </button>
+            </Link>
           </div>
         ) : (
           orgUsers.map((user) => <UserCard key={user.id} user={user} />)
