ad37fdf577321f077cae450ce16ea39e04103e26
Jesse Hardage <j.hardage@pm.me>
2022-08-18 08:59:49 +0100

    commenting out validation for MVP crunch time development

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -9,7 +9,8 @@ async function login (req, res) {
     const email = req.body.email;
     const password = req.body.password;
 
-    if (!validEmail(email) || !validPassword(password)) {
+    // if (!validEmail(email) || !validPassword(password)) {
+    if (!email || !password) {
       res.status(401).send('Invalid fields.');
       return;
     }
