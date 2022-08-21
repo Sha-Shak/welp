47908eaca9548bb1ba65bf1d3ae2e44a0aed19f6
Samiya Kazi <samiya.kazi09@gmail.com>
2022-08-21 14:28:46 +0600

    added badge to contact cards

--- client/src/Components/Chat/ChatWindowNav.jsx
+++ client/src/Components/Chat/ChatWindowNav.jsx
@@ -53,7 +53,8 @@ function ChatWindowNav() {
             </span>
             { contact ? (contact.type === 'admin' ? 
                 <span class="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 ml-3 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800">Admin</span> 
-                : null) : null
+                    : null)
+                : null
             }
             </div>
 
