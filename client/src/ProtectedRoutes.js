a2d038f462af03061bf774d24b87929c3d6570a9
Jesse Hardage <j.hardage@pm.me>
2022-08-21 18:42:18 +0100

    protected routes setup

--- client/src/ProtectedRoutes.js
+++ client/src/ProtectedRoutes.js
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import { useState, useSelector } from 'react-redux';
 import Login from "./pages/Login";
 
 const ProtectedRoutes = () => {
-  const isAuth = true;
-  return isAuth ? <Outlet /> : <Login /> 
+  
+  const user = useSelector((state) => state.auth);
+  return user ? <Outlet /> : <Login /> 
 }
 
 export default ProtectedRoutes;
