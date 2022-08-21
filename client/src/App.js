854cbc545d31c8eee32b28f9cd484457bcc790ba
Sha-Shak <shahriar.shakhawat@gmail.com>
2022-08-21 13:45:05 +0600

    navbar added to all page, app.js cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 import Login from "../src/pages/Login";
-import Sidebar from "./Components/Sidebar/Sidebar";
-import TopBar from "./Components/TopBar/TopBar";
 import AddAdmin from "./pages/AddAdmin";
 import AddUser from "./pages/AddUser";
 import Chat from "./pages/Chat";
 import Dashboard from "./pages/Dashboard";
 import EditAdmin from "./pages/EditAdmin";
 import EditUser from "./pages/EditUser";
+import LandingPage from "./pages/LandingPage";
 import ProfilePage from "./pages/ProfilePage";
 import SignUp from "./pages/SignUp";
 
@@ -17,37 +16,29 @@ function App() {
   return (
     <div data-theme="light" className="App">
       <BrowserRouter>
-        <div className="flex">
-          <Sidebar />
-          <div className="w-full">
-            <TopBar />
-            <div className="container mx-auto">
-              <Routes>
-                {
-                  // <Route path="/" element={<Dashboard />} />
-                }
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<SignUp />} />
-                <Route path="/add-user" element={<AddUser />} />
-                <Route path="/add-admin" element={<AddAdmin />} />
-                <Route path="/edit-admin" element={<EditAdmin />} />
-                <Route path="/edit-user" element={<EditUser />} />
-                <Route path="/chat" element={<Chat />} />
-                <Route path="/dashboard" element={<Dashboard />} />
-                {
-                  //<Route path="/messaging" element={<Messaging />} />
-                }
-                <Route path="/profile" element={<ProfilePage />} />
-                <Route
-                  path="*"
-                  element={
-                    <p>STOP EMBARASSING YOURSELF! Route doesn't exist</p>
-                  }
-                />
-              </Routes>
-            </div>
-          </div>
-        </div>
+        <Routes>
+          <Route path="/land" element={<LandingPage />} />
+          {
+            // <Route path="/" element={<Dashboard />} />
+          }
+
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/add-user" element={<AddUser />} />
+          <Route path="/add-admin" element={<AddAdmin />} />
+          <Route path="/edit-admin" element={<EditAdmin />} />
+          <Route path="/edit-user" element={<EditUser />} />
+          <Route path="/chat" element={<Chat />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          {
+            //<Route path="/messaging" element={<Messaging />} />
+          }
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route
+            path="*"
+            element={<p>STOP EMBARASSING YOURSELF! Route doesn't exist</p>}
+          />
+        </Routes>
       </BrowserRouter>
     </div>
   );
--- client/src/pages/EditAdmin.jsx
+++ client/src/pages/EditAdmin.jsx
@@ -1,17 +1,35 @@
 import React from "react";
+import Sidebar from "../Components/Sidebar/Sidebar";
+import TopBar from "../Components/TopBar/TopBar";
 
 function EditAdmin() {
   return (
-    <div className="flex justify-around">
-      {
-        // <EditAdminForm />
-      }
-      <div>
-        <img
-          src="https://res.cloudinary.com/dgn4bscl4/image/upload/v1660635169/login_don2mi.png"
-          className=""
-          alt=""
-        />
+    <div className="flex">
+      <Sidebar />
+      <div className="w-full">
+        <TopBar />
+        <div className="container mx-auto">
+          <div className="flex">
+            <Sidebar />
+            <div className="w-full">
+              <TopBar />
+              <div className="container mx-auto">
+                <div className="flex justify-around">
+                  {
+                    // <EditAdminForm />
+                  }
+                  <div>
+                    <img
+                      src="https://res.cloudinary.com/dgn4bscl4/image/upload/v1660635169/login_don2mi.png"
+                      className=""
+                      alt=""
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
