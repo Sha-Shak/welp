a40e281e52ce4228b4abcb5953b9214eca540150
Samiya Kazi <samiya.kazi09@gmail.com>
2022-08-21 13:42:20 +0600

    fixed usercard missing info

--- client/src/Components/UserCard/UserCard.jsx
+++ client/src/Components/UserCard/UserCard.jsx
@@ -89,6 +89,8 @@ const UserCard = ({ loggedInUser, user }) => {
             >
               {user.firstname}
             </h2>
+
+            <p className="mb-5 text-xs">{user.interests.join(', ')}</p>
             {user.interest &&
               user.interest.map((interest) => <p>{interest}</p>)}
 
