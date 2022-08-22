b247ea5a2844fc84148aa8b5ad0c31d0dc965322
noel.alam <noel.alam9999@gmail.com>
2022-08-22 15:59:01 +0600

    basic implementation of tags input in dummyform

--- client/src/Components/Inputs/DummyForm/DummyForm.jsx
+++ client/src/Components/Inputs/DummyForm/DummyForm.jsx
@@ -1,71 +1,23 @@
-import React, { Component } from 'react';
-import {COUNTRIES} from './Countries';
-import './style.css';
-import { WithContext as ReactTags } from 'react-tag-input';
+import React, { useState } from "react";
+import { TagsInput } from "react-tag-input-component";
+import "./style.css";
 
-const suggestions = COUNTRIES.map(country => {
-  return {
-    id: country,
-    text: country
-  };
-});
-
-const KeyCodes = {
-  comma: 188,
-  enter: 13
-};
-
-const delimiters = [KeyCodes.comma, KeyCodes.enter];
-
-const DummyForm = () => {
-  const [tags, setTags] = React.useState([
-    { id: 'Thailand', text: 'Thailand' },
-    { id: 'India', text: 'India' },
-    { id: 'Vietnam', text: 'Vietnam' },
-    { id: 'Turkey', text: 'Turkey' }
-  ]);
-
-  const handleDelete = i => {
-    setTags(tags.filter((tag, index) => index !== i));
-  };
-
-  const handleAddition = tag => {
-    setTags([...tags, tag]);
-  };
-
-  const handleDrag = (tag, currPos, newPos) => {
-    const newTags = tags.slice();
-
-    newTags.splice(currPos, 1);
-    newTags.splice(newPos, 0, tag);
-
-    // re-render
-    setTags(newTags);
-  };
-
-  const handleTagClick = index => {
-    console.log('The tag at index ' + index + ' was clicked');
-  };
+export default function App() {
+  const [selected, setSelected] = useState(["papaya"]);
 
   return (
     <div>
-      <h1> React Tags Example </h1>
-      <div>
-        <ReactTags
-          tags={tags}
-          suggestions={suggestions}
-          delimiters={delimiters}
-          handleDelete={handleDelete}
-          handleAddition={handleAddition}
-          handleDrag={handleDrag}
-          handleTagClick={handleTagClick}
-          inputFieldPosition="bottom"
-          autocomplete
-        />
-      </div>
+      <h1>Add Fruits</h1>
+
+      <pre>{JSON.stringify(selected)}</pre>
+
+      <TagsInput
+        value={selected}
+        onChange={setSelected}
+        name="fruits"
+        placeHolder="enter fruits"
+      />
+      <em>press enter to add new tag</em>
     </div>
   );
-};
-
-
-export default DummyForm;
+}
