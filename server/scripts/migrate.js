4aa44c781925eb0bda5a1ddfbfeaaef5cda27933
Samiya Kazi <samiya.kazi09@gmail.com>
2022-08-23 21:05:05 +0600

    c

--- server/scripts/migrate.js
+++ server/scripts/migrate.js
@@ -104,10 +104,6 @@ const messageSql = `
     CONSTRAINT fk_chatid
       FOREIGN KEY (chat_id)
         REFERENCES chatrooms(id)
-        ON DELETE CASCADE,
-    CONSTRAINT fk_senderId
-      FOREIGN KEY (sender_id)
-        REFERENCES users(id)
         ON DELETE CASCADE
   );`
 
