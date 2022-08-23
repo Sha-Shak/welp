8aea6680f3be1e0be22c41a9e483894af8bd857c
Samiya Kazi <samiya.kazi09@gmail.com>
2022-08-23 18:21:09 +0600

    b

--- server/index.js
+++ server/index.js
@@ -42,7 +42,8 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:3000",
-    methods: ['GET', 'POST']
+    methods: ['GET', 'POST'],
+    credentials: true
   }
 })
 
