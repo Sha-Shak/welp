416d401b14baced92f6f359df2b58e8b2e242f5c
Jesse Hardage <j.hardage@pm.me>
2022-08-24 22:30:10 +0100

    minor styling nits

--- client/src/Components/UserCard/CardFlip.jsx
+++ client/src/Components/UserCard/CardFlip.jsx
@@ -13,7 +13,7 @@ const CardFlip = ({ loggedInUser, user }) => {
   const type = loggedInUser.type;
   const admin = type === "admin";
   return (
-    <div className="flip-card w-1/4 mx-2 ">
+    <div className="flip-card w-64 mx-2 ">
       <div className="flip-card-inner">
         <div className="flip-card-front">
           <CardFront currentUser={loggedInUser} user={user} />
