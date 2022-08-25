118d5069d7a47fe236ba372d4231e80316ff6f2d
Sha-Shak <shahriar.shakhawat@gmail.com>
2022-08-25 09:32:42 +0600

    cards improvement

--- client/src/Components/UserCard/CardFlip.jsx
+++ client/src/Components/UserCard/CardFlip.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
+import { useLocation } from "react-router";
 import CardBack from "./CardBack";
 import "./CardFlip.css";
 import CardFront from "./CardFront";
 
 const CardFlip = ({ loggedInUser, user }) => {
   const [front, setFront] = useState(true);
-
+  const location = useLocation();
+  const isDashboard = location.pathname.includes("/dashboard");
   const toggleCard = () => {
     setFront(!front);
   };
@@ -13,7 +15,9 @@ const CardFlip = ({ loggedInUser, user }) => {
   const type = loggedInUser.type;
   const admin = type === "admin";
   return (
-    <div className="flip-card w-64 mx-2 ">
+    <div
+      className={(isDashboard ? "w-1/4 " : "w-full ") + " flip-card mx-2 h-48"}
+    >
       <div className="flip-card-inner">
         <div className="flip-card-front">
           <CardFront currentUser={loggedInUser} user={user} />
--- client/src/Components/UserCard/UserDetails.jsx
+++ client/src/Components/UserCard/UserDetails.jsx
@@ -12,22 +12,21 @@ const UserDetails = ({ user }) => {
   };
 
   return (
-    <div className="py-3">
+    <div>
       <div className="flex flex-row justify-between">
         <p
           onClick={(e) => handleProfile(user.id)}
-          className="card-title w-full text-center text-2xl ml-3 font-bold border-b-2 border-gray-light mb-1 "
+          className="card-title w-full text-2xl ml-3 font-bold border-b-2 border-gray-light mb-1 "
         >
           {user.firstname}
           {user.type === "admin" ? (
-            <span className="bg-indigo/50 text-gray-dark text-xs font-semibold mr-2 px-2.5 py-0.5 rounded">
+            <span class="bg-indigo/50 text-gray-dark text-xs font-semibold mr-2 px-2.5 py-0.5 rounded">
               Admin
             </span>
           ) : null}
         </p>
       </div>
       <div className="py-2">
-        <p className="mb-2 ml-5 text-xs font-bold text-indigo">Bio</p>
         <p className="mb-1 ml-5 text-xs">
           {user.bio ? user.bio : "You know, I'm kind of a private person"}
         </p>
@@ -35,19 +34,13 @@ const UserDetails = ({ user }) => {
           //  <p className="mb-1 ml-5 text-xs font-bold">Location</p>
           // <p className="mb-1 ml-5 text-xs">{user.location ? user.location : 'Earth'}</p>
         }
-        <p className="mb-3 mt-1 ml-5 text-xs font-bold text-indigo">
+        <p className="mb-1 ml-5 text-md text-indigo font-bold border-b-2 border-gray-light">
           Interests
         </p>
         <p className="mb-1 ml-5 text-xs">
           {user.interests.length
-            ? user.interests.map((interest, i) => (
-                <span className="mx-2 my-2 bg-indigo w-auto text-gray-xlight rounded-full py-1 px-2 mr-1">
-                  {interest}
-                </span>
-              ))
-            : //user.interests.slice(0, 3).join
-
-              "Some stuff, you wouldn't understand"}
+            ? user.interests.slice(0, 3).join(", ")
+            : "Some stuff, you wouldn't understand"}
         </p>
       </div>
     </div>
